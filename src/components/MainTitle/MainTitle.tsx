47fdd00b8270276e93205db343ec59e5c37546f3
Anton Kotorovych <akotorovych@gmail.com>
2024-05-22 22:46:08 +0300

    add env api link

--- src/components/MainTitle/MainTitle.tsx
+++ src/components/MainTitle/MainTitle.tsx
@@ -1,14 +1,13 @@
 import { StyledDiv, StyledImg, StyledSection } from './styled';
 
+const API = import.meta.env.VITE_CHUCK_NORRIS_API;
+
 export default function MainTitle() {
   return (
     <StyledSection>
       <StyledDiv>
         <h1>Chuck Norris Joke Generator</h1>
-        <a
-          href="https://api.chucknorris.io/"
-          target="_blank"
-          rel="noopener noreferrer">
+        <a href={API} target="_blank" rel="noopener noreferrer">
           <StyledImg
             src="../assets/chuck.png"
             alt="Chuck Norris pic"
